7a7a3cecc4a493f43f8794198d23274945b17c15
Aditya Raj <adi.adityakohli2005@gmail.com>
2025-02-01 20:16:22 +0530

    ..

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,28 +1,44 @@
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
+// import { Inter } from "next/font/google";
+// import "./globals.css";
+// import { ChatProvider } from "@/context/ChatContext";
+
+// const inter = Inter({ subsets: ["latin"] });
+
+// export const metadata: Metadata = {
+//   title: "code-assistant: A Real-time AI Powered Code Editor",
+//   description: "A real-time code editor",
+// };
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="en">
+//       <ChatProvider>
+//         <body className={inter.className}>{children}</body>
+//       </ChatProvider>
+//     </html>
+//   );
+// }
+"use client";
+import { SessionProvider } from "next-auth/react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ChatProvider } from "@/context/ChatContext";
-import { Providers } from "./Providers"; // Import the Providers component
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "code-assistant: A Real-time AI Powered Code Editor",
-  description: "A real-time code editor",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: any }) {
   return (
     <html lang="en">
-      <ChatProvider>
-        <body className={inter.className}>
-          <Providers>{children}</Providers>
-        </body>
-      </ChatProvider>
+      <SessionProvider>
+        <ChatProvider>
+          <body className={inter.className}>{children}</body>
+        </ChatProvider>
+      </SessionProvider>
     </html>
   );
 }

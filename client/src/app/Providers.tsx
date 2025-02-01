c867211a3197a849d6e60b3ca970ddeaced1d3d2
Aditya Raj <adi.adityakohli2005@gmail.com>
2025-02-01 20:16:22 +0530

    ..

--- client/src/app/Providers.tsx
+++ client/src/app/Providers.tsx
@@ -12,15 +12,15 @@
 // }
 
 
-"use client";
+// "use client";
 
-import { SessionProvider } from "next-auth/react";
-import { ReactNode } from "react";
+// import { SessionProvider } from "next-auth/react";
+// import { ReactNode } from "react";
 
-interface ProvidersProps {
-  children: ReactNode;
-}
+// interface ProvidersProps {
+//   children: ReactNode;
+// }
 
-export function Providers({ children }: ProvidersProps) {
-  return <SessionProvider>{children}</SessionProvider>;
-}
+// export function Providers({ children }: ProvidersProps) {
+//   return <SessionProvider>{children}</SessionProvider>;
+// }

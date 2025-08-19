2c9c8c5131d236a61a195c67324bf085cf7b09f0
shivapreetham <2005shivapreetham@gmail.com>
2025-08-20 02:55:53 +0530

    docker file changes

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World from Java!");
+        System.out.println("Current time: " + System.currentTimeMillis());
+    }
+}
---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <ctime>
+
+int main() {
+    std::cout << "Hello World from C++!" << std::endl;
+    std::cout << "Current timestamp: " << time(0) << std::endl;
+    return 0;
+}
